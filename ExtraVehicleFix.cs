48b76ede939f723639efe5787028e0e07b2b637e
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2017-12-09 20:30:53 -0800

    first commit

---
+++ ExtraVehicleFix.cs
@@ -0,0 +1,43 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System;
+
+namespace ExtraConcentratedJuice.ExtraVehicleFix
+{
+    public class ExtraVehicleFix : RocketPlugin<ExtraVehicleFixConfig>
+    {
+        public static DateTime lastUpdated;
+        protected override void Load()
+        {
+            Logger.Log("Extra's Wheels Fix Loaded!");
+            lastUpdated = DateTime.Now;
+        }
+        protected override void Unload()
+        {
+           
+        }
+
+        public void FixedUpdate()
+        {
+            if (Level.isLoaded && (DateTime.Now - lastUpdated).TotalSeconds > Configuration.Instance.update_interval)
+            {
+                FixTires();
+            }
+        }
+
+        public void FixTires()
+        {
+            foreach (InteractableVehicle v in VehicleManager.vehicles)
+            {
+                foreach(Wheel w in v.tires)
+                {
+                    if (!w.isAlive)
+                    {
+                        w.askRepair();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ExtraVehicleFixConfig.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtraConcentratedJuice.ExtraVehicleFix
+{
+    public class ExtraVehicleFixConfig : IRocketPluginConfiguration
+    {
+        public int update_interval;
+        public void LoadDefaults()
+        {
+            update_interval = 1;
+        }
+    }
+}
